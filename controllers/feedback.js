41b8945cf0883bd95561716ee833da06806311e5
ezequiel <ezeedge@gmail.com>
2020-09-30 17:25:49 -0300

    modificacion

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -7,8 +7,31 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 exports.emailFeedback = (req,res)=> {
     console.log(req.body)
 
-    const {name , email , message, phone , uploadedFiles} = req.body
+   // const {name , email , message, phone , uploadedFiles} = req.body   version anterior
 
+     const {name , email , message} = req.body
+
+    const emailData = {
+        to : process.env.EMAIL_TO ,
+        from : email,
+        subject: 'feedback from',
+        html: `
+
+        <h1>Customer feedback form</h1>
+
+        <hr/>
+
+        <h2>Sender name ${name}<h2/>
+        <h2>Sender email ${email}<h2/>
+        <p>Sender message ${message}<p/>
+  
+        <p>https://febackonline.com</p>
+
+        
+        `
+
+    }
+/*  version anterior
     const emailData = {
         to : process.env.EMAIL_TO ,
         from : email,
@@ -34,6 +57,8 @@ exports.emailFeedback = (req,res)=> {
         `
 
     }
+
+*/
 
     sgMail.send(emailData)
         .then(sent => {
